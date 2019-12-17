8a3277c31d0abb0364178d3469a8912519cfb76a
yuni05902 <gyuni05902@gmail.com>
2019-12-17 23:41:21 +0900

    urls collect

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('bookmark/', include('bookmark.urls')),
+    path('', include('bookmark.urls')),
     path('admin/', admin.site.urls),
 ]
